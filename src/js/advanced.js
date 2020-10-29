e14be6d7c011c9659783705790afab5bfc1eb085
Riya <hiyoriorria@gmail.com>
2020-10-30 00:25:07 +0900

    微細な変更

--- src/js/advanced.js
+++ src/js/advanced.js
@@ -1,15 +1,14 @@
-// JavaScript Document
 import './../scss/advanced.scss';
-$(function () {
-  $('#link-difficulty').click(function () {
-    if ($('.links').hasClass('air')) {
-      $('.air').removeClass('air');
-      $('.links').css("display", "none");
-      $('.btn-link-difficulty').text('他の難易度へ');
-    } else {
-      $('.links').addClass('air');
-      $('.links').css("display", "inline-block");
-      $('.btn-link-difficulty').text('閉じる');
-    }
-  });
+$(function() {
+    $('#link-difficulty').click(function() {
+        if ($('.links').hasClass('air')) {
+            $('.air').removeClass('air');
+            $('.links').css("display", "none");
+            $('.btn-link-difficulty').text('他の難易度へ');
+        } else {
+            $('.links').addClass('air');
+            $('.links').css("display", "inline-block");
+            $('.btn-link-difficulty').text('閉じる');
+        }
+    });
 });
